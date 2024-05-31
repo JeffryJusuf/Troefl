4b53998f61967d5b26680bca3b191ccc4c222b00
Jeffry <jeffry.jusuf.24@gmail.com>
2024-05-31 09:09:43 +0700

    Small fix to home page buttons which direct user to discussion page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,9 @@
             @auth
             <div class="col-lg-7 col-md-8 mx-auto">
                     <h1 class="mt-3 mb-5">Welcome to <strong class="fw-bold">TROEFL</strong>, {{ auth()->user()->username }}</h1>
-                    <p class="lead py-5 border-top border-bottom">Test & Improve your English Skill by Trying our Online English Test with TOEFL Standard</p>
+                    <p class="lead py-5 border-top border-bottom">
+                        Test & Improve your English Skill by Trying our Online English Test with TOEFL Standard.
+                    </p>
                     <p class="mt-5">
                         <a href="/material" class="btn btn-lg btn-secondary">
                             <small class="fs-6">
@@ -18,7 +20,7 @@
                                 Test Your English Skill
                             </small>
                         </a>
-                        <a href="/discussion" class="btn btn-lg btn-secondary">
+                        <a href="/discussions" class="btn btn-lg btn-secondary">
                             <small class="fs-6">
                                 Visit the Discussion Forum
                             </small>
@@ -43,7 +45,7 @@
                                 Learn the Basics
                             </small>
                         </a>
-                        <a href="/discussion" class="btn btn-lg btn-secondary">
+                        <a href="/discussions" class="btn btn-lg btn-secondary">
                             <small class="fs-6">
                                 Visit the Discussion Forum
                             </small>
