797c8ba77445ae01ca651ae41f72e2add04f7788
Jeffry <jeffry.jusuf.24@gmail.com>
2024-06-01 07:34:44 +0700

    Added logo to website's browser tab

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="shortcut icon" type="image/x-icon" href="https://github.com/mdo.png">{{-- temp img til a logo was made --}}
+    <link rel="shortcut icon" type="image/x-icon" href="/img/troefl-logo.png">
     
     @if ($title == 'Register' || $title == 'Login')
         <link rel="stylesheet" href="/css/auth.css">
