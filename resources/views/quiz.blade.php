df358e05777f47be4777b7f4fb5d8397583c5782
Jeffry <jeffry.jusuf.24@gmail.com>
2024-06-10 20:12:15 +0700

    Small change to quiz disclaimer page

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -5,7 +5,7 @@
     <div class="row py-lg-5">
         <div class="col-lg-7 col-md-8 mx-auto">
             <h1 class="mt-0 mb-5">Disclaimer</h1>
-            <p class="lead py-5 border-top border-bottom">
+            <p class="lead py-5 border-top border-bottom border-dark-subtle">
                 This quiz is purely for learning/training purpose and does not represent an official TOEFL test. We do not guarantee you will get a good result on the actual TOEFL test if you get a perfect score on our quiz.
             </p>
             @auth
