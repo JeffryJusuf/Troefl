2a1c3e62fb2594a6ee97af23cc6f2ad923d883e0
Jeffry <jeffry.jusuf.24@gmail.com>
2024-06-10 20:10:13 +0700

    Changes to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,22 +5,22 @@
         <div class="row py-lg-5">
             @auth
                 <div class="col-lg-7 col-md-8 mx-auto">
-                    <h1 class="mt-3 mb-5">Welcome to <img src="/img/troefl-text.png" alt="" width="170"> , {{ auth()->user()->username }}</h1>
-                    <p class="lead py-5 border-top border-bottom">
+                    <h1 class="mt-3 mb-5">Welcome to <img src="/img/troefl-text-dark.png" alt="" width="170"> , {{ auth()->user()->username }}</h1>
+                    <p class="lead py-5 border-top border-bottom border-dark-subtle">
                         Test & Improve your English Skill by Trying our Online English Test with TOEFL Standard.
                     </p>
                     <p class="mt-5">
-                        <a href="/learning-material" class="btn btn-lg btn-secondary">
+                        <a href="/learning-material" class="btn btn-lg btn-dark">
                             <small class="fs-6">
                                 Learn the Basics
                             </small>
                         </a>
-                        <a href="/quiz" class="btn btn-lg btn-secondary mx-1">
+                        <a href="/quiz" class="btn btn-lg btn-dark mx-1">
                             <small class="fs-6">
                                 Test Your English Skill
                             </small>
                         </a>
-                        <a href="/discussions" class="btn btn-lg btn-secondary">
+                        <a href="/discussions" class="btn btn-lg btn-dark">
                             <small class="fs-6">
                                 Visit the Discussion Forum
                             </small>
@@ -29,23 +29,23 @@
                 </div>
             @else
                 <div class="col-lg-7 col-md-8 mx-auto">
-                    <h1 class="mt-0 mb-5">Welcome to <img src="/img/troefl-text.png" alt="" width="170"></h1>
-                    <p class="lead pt-5 border-top">
+                    <h1 class="mt-0 mb-5">Welcome to <img src="/img/troefl-text-dark.png" alt="" width="170"></h1>
+                    <p class="lead pt-5 border-top border-dark-subtle">
                         Test & Improve your English Skill by Trying our Online English Test with TOEFL Standard.
                     </p>
                     <p class="lead py-3">
                         Looks like you aren't signed in yet. <a href="/login" class="text-decoration-none">Sign in now.</a>
                     </p>
-                    <p class="lead pb-5 border-bottom">
+                    <p class="lead pb-5 border-bottom border-dark-subtle">
                         Don't have an account? <a href="/register" class="text-decoration-none">Register now.</a>
                     </p>
                     <p class="mt-5">
-                        <a href="/material" class="btn btn-lg btn-secondary">
+                        <a href="/material" class="btn btn-lg btn-dark">
                             <small class="fs-6">
                                 Learn the Basics
                             </small>
                         </a>
-                        <a href="/discussions" class="btn btn-lg btn-secondary">
+                        <a href="/discussions" class="btn btn-lg btn-dark">
                             <small class="fs-6">
                                 Visit the Discussion Forum
                             </small>
